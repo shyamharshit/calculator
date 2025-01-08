99399d11f6d0ec64fc8d83db8903890f68373858
shyamharshit <shyamfreefire499@gmail.com>
2025-01-08 19:43:36 +0530

    vs code part3

--- app.js
+++ app.js
@@ -17,10 +17,67 @@ else{
 }
 }
 function getFormattedNumber(num){
-    if(num=="_"){
+    if(num=="-"){
         return "";
     }
     var n = Number(num);
     var value = n.toLocaleString("en");
     return value;
 }
+function reverseNumberFormat(num){
+    return Number(num.replace(/,/g,''));
+}
+var operator = Document.getElementByClassName("operator");
+for(var i =0;i<operator.length;i++) {
+    operator[i].addEventListener('click',function(){
+      if (this.id=="clear"){
+        printHistory("");
+        printOutput("");
+      }
+else if(this.id=="backspace"){
+    var output=reverseNumberFormat(getOutput()).toString;
+    if(output){
+        output= output.substr(0,output.length-1);
+        printOutput(output);
+    }
+}
+else{ 
+    var output=getOutput();
+    var history=getHistory;
+        if (output==""&&history!=""){
+            if(isNaN(history[history.length-1])){
+                history=history.substr(0,history.length-1);
+            }
+        } 
+         if(output!="" || history!=""){
+            output= output==""?output:reverseNumberFormat(output);
+            history=history+output;
+            if(this.id=="="){
+                var result=eval(history);
+                printOutput(result);
+                printHistory("");
+            }
+            else{
+                history=history+this.id;
+                printHistory(history);
+                printOutput("");
+            }
+         }   
+        
+     }
+
+    });
+
+}
+var Number = document.getElementsByClassName("number");
+for(var i =0;i<Number.length;i++){
+    number[i].addEventListener('click',function(){
+        var output=reverseNumberFormat(getOutput());
+        if(output!=NaN){
+            output=output+this.id;
+            printOutput(output);
+        }
+    });
+       
+  }
+
